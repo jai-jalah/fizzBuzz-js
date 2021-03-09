f8060fcd745dd7f12a983b5f0b02051c5d1720db
Jai Jalah Kumar <jay.jk@hotmail.com>
2021-03-09 12:05:50 +0000

    Add second passing test

--- jasmine/spec/fizzBuzzSpec.js
+++ jasmine/spec/fizzBuzzSpec.js
@@ -1,8 +1,12 @@
-describe("fizzBuzz", function() {
+describe('fizzBuzz', function() {
 
-  it("should return the number given if it is less than 3", function() {
+  it('should return the number given if it is less than 3', function() {
     expect(fizzBuzz(1)).toEqual(1);
     expect(fizzBuzz(2)).toEqual(2);
+  });
+
+  it("should return 'Fizz' if number given is divisible by 3",function() {
+    expect(fizzBuzz(3)).toEqual('Fizz');
   });
 
 });
--- jasmine/src/fizzBuzz.js
+++ jasmine/src/fizzBuzz.js
@@ -2,4 +2,7 @@ var fizzBuzz = function(number) {
   if (number < 3) {
     return number
   }
+  else if (number % 3 === 0) {
+    return 'Fizz'
+  }
 }
