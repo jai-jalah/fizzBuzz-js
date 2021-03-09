6c3c3f487dc30359cd9519a8286c29921ab721ed
Jai Jalah Kumar <jay.jk@hotmail.com>
2021-03-09 12:18:08 +0000

    Tests now Fulfil all criteria in plans

--- jasmine/spec/fizzBuzzSpec.js
+++ jasmine/spec/fizzBuzzSpec.js
@@ -1,24 +1,25 @@
 describe('fizzBuzz', function() {
 
-  it('should return the given number if it is less than 3', function() {
-    expect(fizzBuzz(1)).toEqual(1);
-    expect(fizzBuzz(2)).toEqual(2);
-  });
-
-  it("should return 'Fizz' if given number is divisible by 3",function() {
+  it("should return 'Fizz' - if given number is divisible by 3",function() {
     expect(fizzBuzz(3)).toEqual('Fizz');
+    expect(fizzBuzz(33)).toEqual('Fizz');
   });
 
-  it("should return 'Buzz' if given number is divisible by 5", function() {
+  it("should return 'Buzz' - if given number is divisible by 5", function() {
     expect(fizzBuzz(5)).toEqual('Buzz');
+    expect(fizzBuzz(20)).toEqual('Buzz')
+    expect(fizzBuzz(100)).toEqual('Buzz')
   });
 
-  it("should return 'FizzBuzz' if given number is divisible by both 3 & 5", function() {
+  it("should return 'FizzBuzz' - if given number is divisible by both 3 & 5", function() {
     expect(fizzBuzz(15)).toEqual('FizzBuzz');
+    expect(fizzBuzz(30)).toEqual('FizzBuzz');
   });
 
-  it('should return the given number if it does not divide by 3, 5, or both', function() {
+  it('should return the given number - if it does not divide by 3, 5, or both', function() {
+    expect(fizzBuzz(1)).toEqual(1);
     expect(fizzBuzz(26)).toEqual(26)
+    expect(fizzBuzz(44)).toEqual(44)
   });
 
 });
